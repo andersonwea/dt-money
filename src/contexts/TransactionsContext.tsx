374ae14d9ec294def0df917b4fd4c11639bbafd9
chain <klaus.mika1052@gmail.com>
2023-05-25 21:18:00 -0300

    Performance - Aplicando o useCallback

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
 import { api } from '../lib/api'
 import { createContext } from 'use-context-selector'
 
@@ -34,7 +34,7 @@ export const TransactionsContext = createContext({} as TransactionsContextType)
 export function TransactionProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('transactions', {
       params: {
         _sort: 'createdAt',
@@ -44,9 +44,9 @@ export function TransactionProvider({ children }: TransactionsProviderProps) {
     })
 
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransaction(data: NewTransactionInput) {
+  const createTransaction = useCallback(async (data: NewTransactionInput) => {
     const { description, price, category, type } = data
 
     const response = await api.post('transactions', {
@@ -58,7 +58,7 @@ export function TransactionProvider({ children }: TransactionsProviderProps) {
     })
 
     setTransactions((state) => [response.data, ...state])
-  }
+  }, [])
 
   useEffect(() => {
     fetchTransactions()
